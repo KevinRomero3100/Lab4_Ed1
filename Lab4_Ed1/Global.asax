72ee957865a219af77706cb4a9b7aaafc6aab4d4
KevinRomero3100 <kevinromero3100@icloud.com>
2020-04-15 14:10:52 -0600

    HashTable

---
+++ Lab4_Ed1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab4_Ed1.MvcApplication" Language="C#" %>
---
+++ Lab4_Ed1/Helpers/Storage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab4_Ed1.Helpers
+{
+    public class Storage
+    {
+        private static Storage _instance = null;
+
+        public static Storage Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Storage();
+                return _instance;
+            }
+        }
+        
+    }
+}
